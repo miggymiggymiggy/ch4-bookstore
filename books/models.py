95161c79c5678bc75b8b05947d9aa2a5dbdbe8ba
Miguel Pascua <themapascua@gmail.com>
2024-08-20 20:35:56 +0800

    'ch13'

--- books/models.py
+++ books/models.py
@@ -14,6 +14,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to="covers/", blank=True)
     
     def __str__(self):
         return self.title
@@ -35,3 +36,7 @@ class Review(models.Model):
     
     def __str__(self):
         return self.review
+    
+    
+#f we wanted to allow uploads of a regular file rather than an image file the only difference
+#could be to change ImageField to FileField
