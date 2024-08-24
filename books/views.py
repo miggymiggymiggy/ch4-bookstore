d8d6727835c7cde4f6077b57179b38c6fca63afb
Miguel Pascua <themapascua@gmail.com>
2024-08-24 21:19:38 +0800

    'ch14'

--- books/views.py
+++ books/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from .models import Book
+from django.contrib.auth.mixins import ( LoginRequiredMixin, PermissionRequiredMixin)
 from django.views.generic import ListView, DetailView
+
+from .models import Book
 # Create your views here.
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = "book_list"
     template_name = "book/book_list.html"
+    login_url = "account_login"
     
-class BookDetailView(DetailView): # new
+class BookDetailView(
+    LoginRequiredMixin, 
+    PermissionRequiredMixin,
+    DetailView):
     model = Book
+    context_object_name = "book"
     template_name = "books/book_detail.html"
+    login_url = "account_login"
+    permission_required = "books.special_status"
--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import os
 
 env = Env()
 env.read_env()
